fe81ef568ec574dec502d04709f57f18a425a97b
Timon Kisera <timonmarick@gmail.com>
2024-12-03 13:41:32 +0300

    Complete DB Setup

--- website/__init__.py
+++ website/__init__.py
@@ -3,10 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 import pymysql
 import random
 import string
+from os import path
 
 #Initializing Database Connection
 db = SQLAlchemy()
-DB_NAME = "WebWithFlask"
+DB_NAME = "webwithflask.db"
 
 #Initializing Flask
 def create_app():
@@ -27,7 +28,6 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
-
     #Importing blueprints
     from .views import views
     from .auth import auth
@@ -36,4 +36,13 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    #Implementing database
+    from .models import User, Note
+
     return app
+
+#Database creation
+def create_database(app):
+    if not path.exists('website/ '+DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
