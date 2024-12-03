e5b1d5949a7ac81573218259ded393b75bd32b41
Timon Kisera <timonmarick@gmail.com>
2024-12-03 22:42:54 +0300

    Static JS To Request Note Deletion

---
+++ website/static/index.js
@@ -0,0 +1,10 @@
+function deletenote(noteID)
+{
+    fetch('/delete-note', {
+        method: 'POST',
+        body: JSON.stringify({noteID: noteID}),
+    })
+    .then((_res) => {
+        window.location.href = "/";
+    })
+}
