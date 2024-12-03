67b0b741da70d6c5cfd1324e39e492b6d52ccb14
Timon Kisera <timonmarick@gmail.com>
2024-12-03 21:35:50 +0300

    Modified Page Access Priviledges

--- website/views.py
+++ website/views.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
 
 #Defining the file as a blueprint
 views = Blueprint('views', __name__)
 
 #Homepage URL
 @views.route('/')
+@login_required
 def homepage():
     return render_template("home.html")
