5a5ec0eb38fe4a60af88bc30fc6d52c14b1a477d
Timon Kisera <timonmarick@gmail.com>
2024-12-03 20:54:25 +0300

    Creating Database File

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from website import create_app
+from website import create_app, create_database
 
 #Calling initializer function
 app = create_app()
 
 #Running web server
 if __name__ == '__main__':
+    with app.app_context():
+        create_database(app)
     app.run(debug=True)
